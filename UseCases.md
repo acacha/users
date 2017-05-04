ec32ba3bb1e94ee50e33edbe48714af38fca6e55
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-05-04 10:11:30 +0200

    Sync package

---
+++ UseCases.md
@@ -0,0 +1,6 @@
+Users Managment
+
+URL: /managment/users
+
+- Regular user cannot access (403 unauthorized) users managment
+- Admin user can access users managment
---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+./sandbox_setup.sh
+cd sandbox
+php artisan dusk:install
+php artisan serve --env=dusk.local &
+php artisan dusk
+php artisan adminlte:admin
+./vendor/bin/phpunit --coverage-text --coverage-clover=../coverage.clover
+#./vendor/bin/phpunit --group=failing
+cd ..
+./sandbox_destroy.sh
+
