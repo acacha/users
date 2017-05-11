54778197d0e3c0839babd0357323417268ee7f0e
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-05-11 11:51:21 +0200

    Sync

--- src/helpers.php
+++ src/helpers.php
@@ -1,16 +1,50 @@
 <?php
 
+use Spatie\Permission\Exceptions\PermissionAlreadyExists;
+use Spatie\Permission\Exceptions\RoleAlreadyExists;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
 
+if (! function_exists('role_first_or_create')) {
+    /**
+     * Create  role by name or retrieve role if already exists.
+     * @param $role
+     * @return \Illuminate\Database\Eloquent\Model|\Spatie\Permission\Contracts\Role|Role
+     */
+    function role_first_or_create($role)
+    {
+        try {
+            return Role::create(['name' => $role]);
+        } catch (RoleAlreadyExists $e) {
+            return Role::findByName($role);
+        }
+    }
+}
+
+if (! function_exists('permission_first_or_create')) {
+    /**
+     * Create permission by name or retrieve permission if already exists.
+     *
+     * @param $permission
+     * @return \Illuminate\Database\Eloquent\Model|\Spatie\Permission\Contracts\Permission
+     */
+    function permission_first_or_create($permission)
+    {
+        try {
+            return Permission::create(['name' => $permission]);
+        } catch (PermissionAlreadyExists $e) {
+            return Permission::findByName($permission);
+        }
+    }
+}
+
 if (! function_exists('initialize_users_management_permissions')) {
     function initialize_users_management_permissions()
     {
-        $manageUsers = Role::fistOrCreate(['name' => 'manage-users']);
-        Permission::fistOrCreate(['name' => 'list-users']);
-//        Permission::create(['name' => 'create-user']);
-        Permission::fistOrCreate(['name' => 'send-user-invitations']);
+        $manageUsers = role_first_or_create('manage-users');
+        permission_first_or_create('list-users');
+        permission_first_or_create('send-user-invitations');;
 
         $manageUsers->givePermissionTo('list-users');
         $manageUsers->givePermissionTo('send-user-invitations');
