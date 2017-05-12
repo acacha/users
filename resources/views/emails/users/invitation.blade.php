e1561e864bc664e8b9d60ea5ff9666953b81dac9
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2017-05-12 14:22:59 +0200

    sync

--- resources/views/emails/users/invitation.blade.php
+++ resources/views/emails/users/invitation.blade.php
@@ -1,1 +1,14 @@
-PROVA!
+@component('mail::message')
+# You have been invited to {{ config('app.name') }} app
+
+Somebody has invited you to use {{ config('app.name') }} app.
+
+Please click on the following link to create a user:
+
+@component('mail::button', ['url' => env('APP_URL') . '/token=' . $invitation->token])
+Register
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
